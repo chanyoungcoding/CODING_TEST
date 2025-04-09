4bedb13e56c1af1c7d8b7691062e95b918927ec7
chanyoungcoding <white1614@naver.com>
2025-04-09 21:37:59 +0900

    test

--- my_solved/Test/Test.java
+++ my_solved/Test/Test.java
@@ -3,39 +3,35 @@ package Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Test {
 
   public static void main(String[] args) throws IOException {
-    
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int N = Integer.parseInt(br.readLine());
+    while (true) {
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    ArrayList<Integer> result = new ArrayList<>();
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      int z = Integer.parseInt(st.nextToken());
 
-    for (int i = 0; i < N; i++) {
-      int num = Integer.parseInt(br.readLine());
-      if(num == 0) {
-        if(!result.isEmpty()) {
-          result.remove(result.size() - 1);
-        }
+      // 0 0 0 을 입력받으면 종료
+      if (x == 0 && y == 0 && z == 0)
+        break;
+
+      if ((x * x + y * y) == z * z) {
+        System.out.println("right");
+      } else if (x * x == (y * y + z * z)) {
+        System.out.println("right");
+      } else if (y * y == (z * z + x * x)) {
+        System.out.println("right");
       } else {
-        result.add(num);
+        System.out.println("wrong");
       }
     }
-
-    int sum = 0;
-
-    for(int value : result) {
-      sum += value;
-    }
-
-    System.out.println(sum);
-
-
   }
 
-  
 }
